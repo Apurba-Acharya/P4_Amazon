28ea0e273db7f5f9a77ae51d489f57ae7c887160
Apurba-Acharya <apurba1907@gmail.com>
2026-01-27 08:12:17 +0530

    27/01

---
+++ src/test/java/runners/amazonProductPurchesFlow.java
@@ -0,0 +1,15 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features/productSearchAndFilter.feature",
+        glue = {"stepDefinitions", "hooks"},
+        plugin = {
+                "pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
+)
+public class amazonProductPurchesFlow extends AbstractTestNGCucumberTests {
+}
